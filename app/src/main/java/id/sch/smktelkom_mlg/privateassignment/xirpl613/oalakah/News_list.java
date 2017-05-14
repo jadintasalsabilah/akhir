362c4ea644c0e3e4b4a84778f330ef23a559c12d
jadintasalsabilah <jadintasalsabilahh@gmail.com>
2017-05-14 11:50:07 +0700

    nambain api, volley,glide

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl613/oalakah/News_list.java
@@ -0,0 +1,32 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl613.oalakah;
+
+import java.io.Serializable;
+
+/**
+ * Created by Smktelkom on 5/14/2017.
+ */
+
+public class News_list implements Serializable {
+    private String image;
+    private String judul;
+    private String popular;
+
+    public News_list(String image, String judul, String popular) {
+        this.image = image;
+        this.judul = judul;
+        this.popular = popular;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public String getPopular() {
+        return popular;
+    }
+
+}
